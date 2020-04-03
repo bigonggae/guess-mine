8a29e3d2a892fd4390520e30bfca190ef6b6f5b3
Hans <hans870709@gmail.com>
2020-04-03 21:12:22 +0900

    Chat with SockeIO

--- src/server.js
+++ src/server.js
@@ -20,5 +20,13 @@ const io = socketIO.listen(server);
 let sockets = [];
 
 io.on("connection", socket => {
-  socket.on("helloGuys", () => console.log("the client said hello"));
+  socket.on("newMessage", ({ message }) => {
+    socket.broadcast.emit("messageNotif", {
+      message,
+      nickname: socket.nickname || "Anon"
+    });
+  });
+  socket.on("setNickname", ({ nickname }) => {
+    socket.nickname = nickname;
+  });
 });
