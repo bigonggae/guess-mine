2d68a90cb04836470f40e15e7ce997c4347372f0
Hans <hans870709@gmail.com>
2020-04-04 22:12:08 +0900

    Login Flow part Two

---
+++ src/socketController.js
@@ -0,0 +1,8 @@
+const socketContoller = socket => {
+  socket.on("setNickname", ({ nickname }) => {
+    console.log(nickname);
+    socket.nickname = nickname;
+  });
+};
+
+export default socketContoller;
