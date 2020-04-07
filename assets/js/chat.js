989efdae641bcd5f75a91e5510c469ba2103f76c
Hans <hans870709@gmail.com>
2020-04-07 21:43:33 +0900

    Realtime Painting part One

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -6,10 +6,10 @@ const sendMsg = document.getElementById("jsSendMsg");
 const appendMsg = (text, nickname) => {
   const li = document.createElement("li");
   li.innerHTML = `
-        <span class="author ${nickname ? "out" : "self"}">${
+          <span class="author ${nickname ? "out" : "self"}">${
     nickname ? nickname : "You"
   }:</span> ${text}
-        `;
+      `;
   messages.appendChild(li);
 };
 
